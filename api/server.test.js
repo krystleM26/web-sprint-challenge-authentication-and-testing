bdc5dd2bdbf17ac6bf544f02d913ff4c498d8437
Krystle Mitchell <mitchell.krystle2@gmail.com>
2022-03-31 22:20:28 -0700

    Created two tests

--- api/server.test.js
+++ api/server.test.js
@@ -1,5 +1,24 @@
 // Write your tests here
+const db = require('../data/dbConfig')
+const server = require('./server')
+const request = require('supertest')
 
+beforeAll(async () => {
+ await db.migrate.rollback();
+ await db.migrate.latest();
+
+})
+
+afterAll(async () => {
+  await db.destroy()
+})
 test('sanity', () => {
   expect(1).toBe(1)
+})
+
+test('[Get] all jokes', async () => {
+  const res = await request(server).get('/api/jokes')
+  expect(res.status).toBe(200);
+  // expect(res.body).toEqual([]);
+  
 })
